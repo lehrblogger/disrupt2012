07bbc58fc0bbc4c0eafe4e612a4e4da805efda85
Steven Lehrburger <lehrburger@gmail.com>
2012-05-19 16:30:50 -0400

    testing receopt of checkin pushes

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World!'
+    
+@app.route('/foursquare/push', methods=['POST'])
+def checkin_push():
+    if request.method == 'POST':
+        app.logger.error('received a post')
+        app.logger.error(request.form['secret'])
+        app.logger.error(request.form['checkin'])
+    else:
+        app.logger.error('not a post')
+    return 'ok'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
