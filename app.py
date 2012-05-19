739a6848a4181c159218dd3175994abd83179eed
Steven Lehrburger <lehrburger@gmail.com>
2012-05-19 17:10:13 -0400

    Set log level

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def checkin_push():
         return "Invalid push secret", 401
         
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(levelname)-8s %(message)s')
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
