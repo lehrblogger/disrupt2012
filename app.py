51d0af263ff7f1b2b771e46210914ba237221a5e
Steven Lehrburger <lehrburger@gmail.com>
2012-05-19 20:29:07 -0400

    Use nickname if available, check for hashtags

--- app.py
+++ app.py
@@ -17,20 +17,25 @@ def checkin_push():
         conn = None
         try:
             checkin = json.loads(request.form['checkin'])
-            conn = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'], sslmode='require')
-            cur = conn.cursor()
-            cur.execute("SELECT numbers FROM users WHERE foursquare_id=%s;", (checkin['user']['id']))
-            numbers = cur.fetchone()
-            conn.close()
-            if numbers:
-                client = TwilioRestClient(os.environ['TWILIO_ACCOUNT'], os.environ['TWILIO_TOKEN'])
-                for number in numbers[0].split(','):
-                    try:
-                        client.sms.messages.create(to='+1%s' % number, from_=os.environ['TWILIO_OUTGOING'],
-                            body='%s %s just checked in to %s at %s. Why don\'t you head there now?' %
-                            (checkin['user']['firstName'], checkin['user']['lastName'], checkin['venue']['name'], checkin['venue']['address']))
-                    except TwilioRestException, e:
-                        logging.error("Error sending message with Twilio to number %s for user %s: %s" % (number, checkin['user']['id'], e))
+            if checkin['shout'].find('#posse') > 0 or checkin['shout'].find('#p0sse') > 0:
+                conn = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'], sslmode='require')
+                cur = conn.cursor()
+                cur.execute("SELECT nickname, numbers FROM users WHERE foursquare_id=%s;", (checkin['user']['id']))
+                result = cur.fetchone()
+                conn.close()
+                if result and result[1]:
+                    client = TwilioRestClient(os.environ['TWILIO_ACCOUNT'], os.environ['TWILIO_TOKEN'])
+                    if result[0]:
+                        message = '%s just checked in to %s at %s. Why don\'t you head there now?' %
+                            (result[0], checkin['venue']['name'], checkin['venue']['address'])
+                    else:
+                        message = '%s %s just checked in to %s at %s. Why don\'t you head there now?' %
+                            (checkin['user']['firstName'], checkin['user']['lastName'], checkin['venue']['name'], checkin['venue']['address'])
+                    for number in result[1].split(','):
+                        try:
+                            client.sms.messages.create(to='+1%s' % number, from_=os.environ['TWILIO_OUTGOING'], body=message)
+                        except TwilioRestException, e:
+                            logging.error("Error sending message with Twilio to number %s for user %s: %s" % (number, checkin['user']['id'], e))
             return 'Checkin push received successfully', 200
         except Exception, e:
             logging.error("Error processing checkin: %s" % e)
