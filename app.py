c46ba3ffab07051501771ff57d283558b6ab4fcb
Steven Lehrburger <lehrburger@gmail.com>
2012-05-19 21:26:17 -0400

    punctuation hack for venue addresses ending in '.'

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def checkin_push():
                     name_str = "%s %s" % (checkin['user']['firstName'], checkin['user']['lastName'])
                 if 'location' in checkin['venue'] and 'address' in checkin['venue']['location']:
                     address_str = ' at %s' % (checkin['venue']['location']['address'])
-                message = '%s just checked in to %s%s. Why don\'t you head there now?' % (name_str, checkin['venue']['name'], address_str)
+                message = '%s just checked in to %s%s – why don\'t you head there now?' % (name_str, checkin['venue']['name'], address_str)
                 for number in numbers.split(','):
                     try:
                         client.sms.messages.create(to='+1%s' % number, from_=os.environ['TWILIO_OUTGOING'], body=message)
